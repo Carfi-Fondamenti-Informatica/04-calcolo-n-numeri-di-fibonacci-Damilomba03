0aee5de3d4b65d966b156f54ad74259517524e3f
Damilomba03 <115529057+Damilomba03@users.noreply.github.com>
2022-11-23 17:08:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n;
+    int p=1,s=1;
+    cin>>n;
+    if(n>=2)
+    {
+        cout<<p<<endl<<s<<endl;
+        int i=3;
+        while(i++,i<=n)
+        {
+            cout<<p+s<<endl;
+            if(p<s)
+            {
+                p=s+p;
+            }
+            else
+            {
+               s=p+s;
+            }
+        }
+    }
+    else
+    {
+        cout<<"ERRORE";
+    }
+    return 0;
 }
